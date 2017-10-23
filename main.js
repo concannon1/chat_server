7752bdfa0d398aecdc068cc053aa2e674b34ab27
Hugh Concannon <concannh@tcd.ie>
2017-10-23 12:30:07 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+'use strict';
+
+// load the Node.js TCP library
+const net = require('net');
+
+const PORT = 5000;
+const ADDRESS = '127.0.0.1';
+
+let server = net.createServer(onClientConnected);
+server.listen(PORT, ADDRESS);
+
+function onClientConnected(socket) {
+	console.log(`New client: ${socket.remoteAddress}:${socket.remotePort}`);
+	let clientName = socket.remoteAddress+":"+socket.remotePort;
+	socket.write("hi "+clientName+"\n");
+	socket.destroy();
+}
+
+console.log(`Server started at: ${ADDRESS}:${PORT}`);
